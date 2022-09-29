4579743442025deffb4b884f8862913f0af8ed1a
Aromal S <93247039+aromals-github@users.noreply.github.com>
2022-09-29 14:26:55 +0530

    modified log in method

--- base/models.py
+++ base/models.py
@@ -10,8 +10,8 @@ class User(AbstractUser):
 
     avatar =models.ImageField(null=True,default="avatar.svg")
 
-    USERNAME_FIELD = 'email'
-    REQUIRED_FIELDS = []
+    USERNAME_FIELD = 'username'
+    REQUIRED_FIELDS = ['email']
 
 
 class Topic(models.Model):
